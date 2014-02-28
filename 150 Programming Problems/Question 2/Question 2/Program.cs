c58cd6b1eac492eb01d0952e29ed7c0b86b38e85
Joseph Goertzen <joey.goertzen@gmail.com>
2014-02-28 07:02:15 -0700

    150PP Q2 Done

---
+++ 150 Programming Problems/Question 2/Question 2/Program.cs
@@ -0,0 +1,53 @@
+﻿//Write code to reverse a C-Style String. (C-String means that “abcd” is represented as five characters, including the null character.)
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace Question_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] array;
+
+            do
+            {
+                Console.Write("Enter a string: ");
+                array = StringToCString(Console.ReadLine());
+                Console.WriteLine(array);
+                Console.WriteLine(reverse(array));
+            } while (true);
+            
+        }
+
+        static char[] reverse(char[] array)
+        {
+            Stack<char> stack = new Stack<char>();
+            List<char> list = new List<char>();
+
+            foreach (char c in array)
+                if (c != '\0')
+                    stack.Push(c);
+
+            while (stack.Count > 0)
+                list.Add(stack.Pop());
+            
+            list.Add('\0');
+
+            return  list.ToArray();
+        }
+
+        static char[] StringToCString(string s)
+        {
+            char[] array = new char[s.Length + 1];
+            array[array.Length - 1] = '\0';
+            for (int i = 0; i < s.Length; ++i)
+                array[i] = s[i];
+            return array;
+        }
+    }
+}
