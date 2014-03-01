4f0be2a5d6ea24191ff2dac198cc9f210294d3d9
Joseph Goertzen <joey.goertzen@gmail.com>
2014-03-01 07:44:28 -0700

    150PP 5,6

---
+++ 150 Programming Problems/6/6/Program.cs
@@ -0,0 +1,49 @@
+﻿//1.6 Given an image represented by an NxN matrix, where each pixel in the image is 4 bytes, write a method to rotate the image by 90 degrees. Can you do this in place?
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GDIDrawer;
+using System.Drawing;
+
+namespace _6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CDrawer canvas = new CDrawer(150,150);
+            canvas.Scale = 50;
+            
+
+            Color[,] colors = new Color[,] { { Color.Red, Color.Red, Color.Red }, { Color.Green, Color.Green, Color.Green }, { Color.Yellow, Color.Yellow, Color.Yellow } };
+
+            do
+            {
+                for (int row = 0; row < colors.GetLength(0); ++row)
+                    for (int column = 0; column < colors.GetLength(1); ++column)
+                        canvas.SetBBScaledPixel(column, row, colors[row, column]);
+                Console.ReadKey();
+                colors = rotate(colors);
+
+            } while (true);
+            
+
+
+        }
+
+        static Color[,] rotate(Color[,] a)
+        {
+            Color[,] b = new Color[a.GetLength(0), a.GetLength(1)];
+
+            for (int row = 0, column = a.GetLength(1) - 1; row < a.GetLength(0); ++row, --column)
+            {
+                for(int i = 0; i < a.GetLength(0); ++i)
+                    b[i, column] = a[row, i];
+            }
+
+            return b;
+        }
+    }
+}
---
+++ 150 Programming Problems/Question 5/Question 5/Program.cs
@@ -0,0 +1,25 @@
+﻿//1.5 Write a method to replace all spaces in a string with ‘%20’.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Question_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.WriteLine(replace(Console.ReadLine()));
+            } while (true);
+        }
+
+        static string replace(string s)
+        {
+            return s.Replace(" ", "%20");
+        }
+    }
+}
