69927e124af2b3fca9ce10c2357c15fcba22de1a
Joseph Goertzen <joey.goertzen@gmail.com>
2014-03-11 11:07:25 -0600

    MVC Tutorial
    Added a controller.

---
+++ MvcTutorial/MvcTutorial/Controllers/HelloWorldController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcTutorial.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        //
+        // GET: /HelloWorld/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
