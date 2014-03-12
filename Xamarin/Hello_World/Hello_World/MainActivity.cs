3da6ee36f59f781e09ae7c32a36ba36c0742c854
Joseph Goertzen <joey.goertzen@gmail.com>
2014-03-11 18:23:16 -0600

    Xamarin Fun

---
+++ Xamarin/Hello_World/Hello_World/MainActivity.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace Hello_World
+{
+	[Activity (Label = "Hello_World", MainLauncher = true)]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Main);
+
+			var aButton = FindViewById<Button>(Resource.Id.aButton);
+			var aLabel = FindViewById<TextView>(Resource.Id.helloLabel);
+
+			aButton.Click += (sender, e) => {
+				aLabel.Text = "Hello from the button!!!!!!";
+			};
+
+		}
+	}
+}
+
+
